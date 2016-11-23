dec39136a1272094d3089efae03332ff6f1f5e5b
Mate Marschalko <mares0909@gmail.com>
2016-11-23 22:19:49 +0000

    Added blink led example project

---
+++ 1-blink-led/index.js
@@ -0,0 +1,24 @@
+var five = require("johnny-five");
+var arduino = new five.Board();
+
+arduino.on("ready", function() {
+	
+	// The Arduino board is ready
+
+	// Access the LED on pin number 10
+	var led = new five.Led(10);
+
+	// Blink the LED every half second
+	// led.blink(500);
+
+	// Pulsate LED
+	// led.pulse();
+
+	// Fade LED in
+	led.fadeIn();
+
+	// Wait 3 seconds then fade out
+	this.wait(3000, function(){
+	    led.fadeOut();
+	});
+});
