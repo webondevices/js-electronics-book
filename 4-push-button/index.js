c9986d3cc519e539293c0a11d46b899546139f19
Mate Marschalko <mares0909@gmail.com>
2016-11-28 20:58:21 +0000

    Added push button project

---
+++ 4-push-button/index.js
@@ -0,0 +1,24 @@
+var five = require("johnny-five");
+var arduino = new five.Board();
+
+arduino.on("ready", function() {
+    
+    // Access the push button on pin D2
+    var button = new five.Button(2);
+
+    // Event listener with callback function
+    // Will capture button down event
+    button.on('down', function() {
+        console.log('button is pressed');
+    });
+
+    // Will capture button held down event
+    button.on('hold', function() {
+        console.log('button is held down for over half a second');
+    });
+
+    // Will capture button released event
+    button.on('up', function() {
+        console.log('button released');
+    });
+});
