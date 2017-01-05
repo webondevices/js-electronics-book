541cdbf4eef590e1fe559b040fa1b3062d28ee34
Mate Marschalko <webondevices@gmail.com>
2017-01-05 00:27:10 +0000

    Completed plant monitor project

--- 5-plant-monitor/index.js
+++ 5-plant-monitor/index.js
@@ -1,10 +1,56 @@
 var five = require('johnny-five');
+var Twit = require('twit');
+var T = new Twit({
+    consumer_key: '',
+    consumer_secret: '',
+    access_token: '',
+    access_token_secret: ''
+});
+var Twilio = require('twilio')('accounts_id', 'auth_token');
+
 var arduino = new five.Board();
 
 var celsius = 0;
-var fahrenheit = 0;
 var light = 0;
 var moisture = 0;
+
+var sentAlertThisPeriod = {
+    temperature: false,
+    light: false,
+    moisture: false
+};
+
+function sendAlert(message, type) {
+
+    // Text message
+    Twilio.messages.create({
+        to: 'a-verified-phone-number',
+        from: 'your-special-twilio-phone-number',
+        body: message
+    
+    // Handle error messages
+    }, function(error) {
+        console.log(error === null ? 'SMS sent!' : JSON.stringify(error));
+    });
+    
+
+    // Tweet message
+    T.post('statuses/update', {
+        status: message
+
+    // Handle error messages
+    }, function(error) {
+        console.log(error === undefined ? 'Tweet sent!' : JSON.stringify(error));
+    });
+
+    // Disable alerts for the sensor type
+    sentAlertThisPeriod[type] = true;
+
+    // Enable alerts after timeout
+    setTimeout(function(){
+        sentAlertThisPeriod[type] = false;
+    }, 60 * 60 * 1000);
+}
 
 arduino.on('ready', function() {
     
@@ -26,14 +72,41 @@ arduino.on('ready', function() {
 
     thermometer.on('data', function(){
         celsius = this.C;
-        fahrenheit = this.F;
+
+        console.log(celsius);
+
+        // If no alert was sent this hour
+        if (!sentAlertThisPeriod.temperature) {
+            if (celsius > 25) sendAlert('It\'s really hot in here: ' + celsius + '°C', 'temperature');
+            if (celsius < 15) sendAlert('It\'s freezing cold in here: ' + celsius + '°C', 'temperature');
+        }
     });
 
     lightSensor.on('data', function(){        
-        light = this.value;
+        var now = new Date();
+        var currentHour = now.getHours();
+
+        // Convert to percentage
+        light = (this.value / 1024) * 100;
+
+        console.log(light);
+
+        // If no alert was sent this hour
+        // And time is between 6am and 8pm to prevent alerts at night
+        if (!sentAlertThisPeriod.light && currentHour < 20 && currentHour > 6) {
+            if (light < 40) sendAlert('It\'s way too dark in here: ' + light + '%', 'light');
+        }
     });
 
     moistureSensor.on('data', function(){
-        moisture = this.value;
+        // Convert to percentage
+        moisture = (this.value / 1024) * 100;
+
+        console.log(moisture);
+
+        // If no alert was sent this hour
+        if (!sentAlertThisPeriod.moisture) {
+            if (moisture < 25) sendAlert('Water me please! My soil is really dry: ' + moisture + '%', 'moisture');
+        }
     });
 });
