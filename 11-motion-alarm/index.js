36fc33f1a5ca9f950db69f0b39725ad42d4f0c09
Mate Marschalko <webondevices@gmail.com>
2017-02-27 23:02:25 +0000

    Motion alarm example added

---
+++ 11-motion-alarm/index.js
@@ -0,0 +1,24 @@
+var five = require('johnny-five');
+var arduino = new five.Board();
+
+arduino.on('ready', function() {
+    
+    var led = new five.Led(6);
+    var piezo = new five.Piezo(3);
+    var motion = new five.Motion(4);
+
+    var alarmTone = [["C4", 1], ["C3", 1], ["C4", 1], ["C3", 1]];
+
+    motion.on("motionstart", function() {
+        led.blink(250);
+
+        piezo.play({
+            song: alarmTone,
+            tempo: 50
+        });
+    });
+  
+    motion.on("motionend", function() {
+        led.stop().off();
+    });
+});
