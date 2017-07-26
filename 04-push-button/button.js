f7968470cf3c48bf75dc0429c065d5915a89a280
Mate Marschalko <webondevices@gmail.com>
2017-07-27 00:01:05 +0100

    LED added to button example

--- 04-push-button/button.js
+++ 04-push-button/button.js
@@ -7,19 +7,25 @@ arduino.on('ready', function () {
     // Access the push button on pin D2
     var button = new five.Button(2);
 
+    // Access the LED on pin D6
+    var led = new five.Led(6);
+
     // Event listeners with callback functions
     // Will capture button down event
     button.on('down', function () {
         console.log('button is pressed');
+        led.on();
     });
 
     // Will capture button held down event
     button.on('hold', function () {
         console.log('button is held down for over half a second');
+        led.blink(100);
     });
 
     // Will capture button released event
     button.on('up', function () {
         console.log('button is released');
+        led.stop().off();
     });
 });
